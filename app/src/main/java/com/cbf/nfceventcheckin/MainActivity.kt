2aa6215ea90ffd4420e31e710a19b6c5064c58e5
seda <sedakundakitchen@gmail.com>
2024-12-23 09:02:43 +0000

    nfc handling init

--- app/src/main/java/com/cbf/nfceventcheckin/MainActivity.kt
+++ app/src/main/java/com/cbf/nfceventcheckin/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cbf.nfceventcheckin
 
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -11,15 +15,54 @@ import androidx.navigation.compose.rememberNavController
 import com.cbf.nfceventcheckin.ui.theme.NFCEventCheckInTheme
 
 class MainActivity : ComponentActivity() {
+    private var nfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
         setContent {
             NFCEventCheckInTheme {
                 Navigation()
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        enableNfcForegroundDispatch()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disableNfcForegroundDispatch()
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        // todo
+    }
+
+    private fun handleNfcTag(tag: Tag) {
+        Log.d("Main","log tag info ${tag.id}")
+    }
+
+    private fun enableNfcForegroundDispatch() {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+            Log.d("Main", "NFC Tag detected")
+            tag?.let {
+                Log.d("Main", "NFC Tag discovered and handled.")
+                handleNfcTag(it)
+            }
+        }
+    }
+
+    private fun disableNfcForegroundDispatch() {
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
 }
+
 
 @Composable
 fun Navigation() {
