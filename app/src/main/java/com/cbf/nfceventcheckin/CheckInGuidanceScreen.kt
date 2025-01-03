b6e292504987e378ccdb282964578337a3401781
seda <sedakundakitchen@gmail.com>
2025-01-03 14:04:44 +0000

    Modify event list screen to show check in prompt

--- app/src/main/java/com/cbf/nfceventcheckin/CheckInGuidanceScreen.kt
+++ app/src/main/java/com/cbf/nfceventcheckin/CheckInGuidanceScreen.kt
@@ -1,23 +1,37 @@
 package com.cbf.nfceventcheckin
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import android.net.Uri
+import android.view.SurfaceView
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.text.style.TextAlign
-import coil.compose.rememberAsyncImagePainter
-import coil.ImageLoader
-import coil.request.ImageRequest
-import android.os.Build.VERSION.SDK_INT
-import coil.decode.GifDecoder
-import coil.decode.ImageDecoderDecoder
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
 
 @Composable
 fun CheckInGuidanceScreen() {
@@ -28,52 +42,76 @@ fun CheckInGuidanceScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        GifImage(
+        CheckInGuidanceGifImage(
             modifier = Modifier
-                .size(350.dp)
+                .size(380.dp)
+                .padding(16.dp)
+                .clip(CircleShape)
+                .border(4.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
         Spacer(modifier = Modifier.size(32.dp))
         Text(
-            text = "Hold your phone near the reader to check-in",
+            text = "Hold your Phone to the Event Reader to Check In",
             style = MaterialTheme.typography.titleMedium.copy(
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 18.sp
-            ),
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp),
+            color = MaterialTheme.colorScheme.primary,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
             textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onBackground,
             letterSpacing = 0.5.sp
         )
     }
 }
 
 @Composable
-fun GifImage(
+fun CheckInGuidanceGifImage(
     modifier: Modifier = Modifier,
 ) {
     val context = LocalContext.current
-    val imageLoader = ImageLoader.Builder(context)
-        .components {
-            if (SDK_INT >= 28) {
-                add(ImageDecoderDecoder.Factory())
-            } else {
-                add(GifDecoder.Factory())
-            }
-        }
-        .build()
 
-    Image(
-        painter = rememberAsyncImagePainter(
-            ImageRequest.Builder(context)
-                .data(data = R.drawable.nfc_prompt_hand)
-                .build(),
-            imageLoader = imageLoader
-        ),
+    AsyncImage(
+        model = ImageRequest.Builder(context)
+            .data(R.drawable.nfc_prompt)
+            .build(),
         contentDescription = "Scanning animation",
         modifier = modifier,
     )
+}
+
+
+@Composable
+fun CheckInGuidanceVideoPlayer(
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val videoUri: Uri = Uri.parse("android.resource://${context.packageName}/raw/nfc_prompt")
+
+    val exoPlayer = remember(context) {
+        ExoPlayer.Builder(context).build().apply {
+            val mediaItem = MediaItem.fromUri(videoUri)
+            setMediaItem(mediaItem)
+        }
+    }
+
+    LaunchedEffect(exoPlayer) {
+        exoPlayer.prepare()
+        exoPlayer.play()
+    }
+    DisposableEffect(exoPlayer) {
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        AndroidView(factory = {
+            SurfaceView(context).apply {
+                exoPlayer.setVideoSurfaceHolder(holder)
+            }
+        })
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/cbf/nfceventcheckin/EventListScreen.kt
+++ app/src/main/java/com/cbf/nfceventcheckin/EventListScreen.kt
@@ -2,7 +2,9 @@ package com.cbf.nfceventcheckin
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,7 +16,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,18 +40,76 @@ fun EventListScreen(events: List<Event>, navController: NavController) {
             .background(MaterialTheme.colorScheme.background)
     ) {
         LazyColumn(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                ) {
+
+                    CheckInGuidanceVideoPlayer(
+                        modifier = Modifier
+                            .size(350.dp)
+                            .padding(16.dp)
+                            .clip(CircleShape)
+                            .border(4.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                    )
+                    Text(
+                        text = "Hold your Phone to the Event Reader to check-in",
+                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                }
+            }
+
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
+                        thickness = 1.dp
+                    )
+                    Text(
+                        text = "Or",
+                        style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    )
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
+                        thickness = 1.dp
+                    )
+                }
+            }
+
+            item {
                 Text(
-                    text = "Select Your Event",
-                    style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                    text = "Select your Event to check-in",
+                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
                     color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(bottom = 16.dp)
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
             }
+
             items(events) { event ->
-                EventListItem(event, navController)
+                EventListItem(
+                    event = event,
+                    navController = navController
+                )
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
     }
